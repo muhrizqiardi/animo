0050ac4392050283f45086253d32893f640b76c7
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-05-15 04:25:04 +0700

    feat: add handler for POST /api/message-books

---
+++ front-end/src/app/api/message-books/route.ts
@@ -0,0 +1,47 @@
+import * as z from 'zod';
+import { createNewMessageBookResponseSchema } from '@/response-objects/back-end';
+import { env } from '@/utils/env';
+import { registerNewMessageFormDtoSchema } from '@/dtos';
+
+export async function POST(request: Request) {
+  try {
+    const incomingRequestBody = await request.json();
+    registerNewMessageFormDtoSchema.parse(incomingRequestBody);
+
+    const outgoingRequestBody = {
+      name: incomingRequestBody.name,
+    };
+    const backendResponse = await fetch(
+      `${env.BACKEND_API_URL}/message-books`,
+      {
+        method: 'POST',
+        body: JSON.stringify(outgoingRequestBody),
+      },
+    );
+    const backendResponseBody = await backendResponse.json();
+    const newMessageBook = z
+      .object({
+        success: z.boolean(),
+        message: z.string(),
+        data: createNewMessageBookResponseSchema,
+      })
+      .parse(backendResponseBody);
+
+    const repsonseBody = JSON.stringify({
+      success: true,
+      message: 'Successfully added a new message book',
+      data: {
+        id: newMessageBook.data.id,
+        slug: newMessageBook.data.slug,
+        name: newMessageBook.data.name,
+      },
+    });
+
+    return new Response(repsonseBody, {
+      status: 201,
+      headers: {
+        'Set-Cookies': `token=${newMessageBook.data.token}; HttpOnly`,
+      },
+    });
+  } catch (error) {}
+}
