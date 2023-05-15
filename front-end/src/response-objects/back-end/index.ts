74cd7a2fe9339590427375610dc8e0fc188b37aa
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-05-15 12:25:49 +0700

    feat: add response object

--- front-end/src/response-objects/back-end/index.ts
+++ front-end/src/response-objects/back-end/index.ts
@@ -8,3 +8,11 @@ export const createNewMessageBookResponseSchema = z.object({
 });
 export interface CreateNewMessageBookResponse
   extends z.infer<typeof createNewMessageBookResponseSchema> {}
+
+export const createNewResponseResponseSchema = z.object({
+  id: z.string(),
+  content: z.string(),
+  messageBookId: z.string(),
+});
+export interface CreateNewResponseResponse
+  extends z.infer<typeof createNewResponseResponseSchema> {}
